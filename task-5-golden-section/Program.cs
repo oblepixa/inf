02224f196d2cafe65637dc8132484e4cc8aada70
oblepixa <60112915+oblepixa@users.noreply.github.com>
2021-05-13 11:04:25 +0300

    Add files via upload

---
+++ task-5-golden-section/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+namespace task_5_golden_section
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double A = Math.E, B = -1, a = -3, b = 3, eps = 0.000001;
+            double fi = (1 + Math.Sqrt(5)) / 2;
+            double c;
+            double x;
+            Console.WriteLine("Поиск точки экстремума будет осуществляться на отрезке[-3; 3]");
+            while (b - a > eps)
+            {
+                c = a + (b - a) / fi;
+                x = (a + b - c);
+                if (c > x)
+                {
+                    if ((Math.Pow(A, c)) - B * c > (Math.Pow(A, x)) - B * x)
+                    {
+                        b = c;
+                    }
+                    else
+                    {
+                        a = x;
+                    }
+                }
+                else
+                {
+                    if (c < (Math.Pow(A, c)) - B * c)
+                    {
+                        a = c;
+                    }
+                    else
+                    {
+                        b = x;
+                    }
+
+                }
+                Console.WriteLine("Начало отрезка: " + a);
+                Console.WriteLine("Конец отрезка: " + b);
+                Console.WriteLine("Точка экстремума: " + x);
+            }
+
+               
+            
+
+        }
+    }
+}
+
